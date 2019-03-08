ef022fa2e51fab6f5eed49738c82f0d9cdc6c5c9
Vivek Koodli Udupa <vkoodli@login001.palmetto.clemson.edu>
2019-03-08 00:44:53 -0500

    Renamed bully_detect to training

---
+++ detection_Model.py
@@ -0,0 +1,115 @@
+# Importing libraries
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import sys # For command Line arguments
+
+#Defining the CNN
+class CNNModel(nn.Module):
+    """ A CNN Model for image classification """
+    
+    def __init__(self,image_size, op_size):
+        """ CNN layer to process the image"""
+        super(CNNModel, self).__init__() # Super is used to refer to the base class, i.e nn.Module
+
+        # Convolution Layer 1
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+
+        # Max Pooling 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution Layer 2
+        self.cnn2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+        # Max Pooling 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution Layer 3
+        self.cnn3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        
+        # Max Pooling 3
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        
+        # Dropout Regularization
+        self.dropout = nn.Dropout(p=0.5)
+
+        # Fully connected linear layer
+        #self.fc1 = nn.Linear(32*75*75 , 9)  #32 channels, 75x75 final image size
+        self.fc1 = nn.Linear(16*image_size*image_size, 10000)  #32 channels, 7x7 final image size
+        self.relu4 = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        
+        self.fc2 = nn.Linear(10000, 5000)  #32 channels, 7x7 final image size
+        self.relu5 = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        
+        self.fc3 = nn.Linear(5000, 2500)  #32 channels, 7x7 final image size
+        self.relu6 = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        
+        self.fc4 = nn.Linear(2500, 100)  #32 channels, 7x7 final image size
+        self.relu7 = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.3)
+        
+        self.fc5 = nn.Linear(100, 10)  #32 channels, 7x7 final image size
+	
+	#Image size = 28x28 -> 13x13 after first pooling
+	#14x14 after padding = 1
+	#7x7 after second pooling
+
+    def forward(self, x):
+        """ Forward Propogation for classification """
+        
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+        
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        # Convolution 2
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2
+        out = self.maxpool2(out)
+        
+        # Convolution 3
+        out = self.cnn3(out)
+        out = self.relu3(out)
+
+        # Max pool 2
+        out = self.maxpool3(out)
+        
+        # Resize the tensor, -1 decides the best dimension automatically
+        #out = out.view(out.size(0), -1)
+        out = out.view(out.size(0), -1)
+
+        # Dropout
+        out = self.dropout(out)
+
+        # Fully connected 1
+        out = self.fc1(out)
+        out = self.relu4(out)
+
+        out = self.fc2(out)
+        out = self.relu5(out)
+
+        out = self.fc3(out)
+        out = self.relu5(out)
+        out = self.dropout(out)
+
+        out = self.fc4(out)
+        out = self.relu6(out)
+        
+        out = self.fc5(out)
+        
+        # Return
+        return out
